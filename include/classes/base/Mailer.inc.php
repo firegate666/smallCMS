5620ef85d62e03781171d4d9b9be96a28230a31c
firegate666 <firegate666>
2006-08-28 15:34:38 +0000

    error handling and default handling for Mailer::simplesend

--- include/classes/base/Mailer.inc.php
+++ include/classes/base/Mailer.inc.php
@@ -1,8 +1,35 @@
 <?php
+/**
+ * handle mailsending
+ */
 class Mailer {
-	function simplesend($from, $to, $subject, $body){
-		$headers = "From: $from";
-		@mail($to, $subject, $body, $headers);
+
+	/**
+	 * send email
+	 * 
+	 * @param	String	$from	message sender
+	 * @param	String	$to	message receiver
+	 * @param	String	$subject	message subject
+	 * @param	String	$body	message body
+	 * 
+	 * @return	boolean	send result
+	 */
+	function simplesend($from, $to, $subject, $body) {
+		
+		if ($from == '')
+			$from = get_config('sender', false);
+			
+		if ($to == '')
+			$to = get_config('receiver', false);
+		
+		if (($from === false) || ($to === false)) {
+			error('Email could not be send because no system sender and/or receiver is set',
+				'Mailer', 'simplesend',
+				array('from'=>$from, 'to'=>$to, 'subject'=>'subject', 'body'=>$body));
+		} else {
+			$headers = "From: $from";
+			return @mail($to, $subject, $body, $headers);
+		}
 	}
 }
 ?>
