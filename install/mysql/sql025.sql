653992edd8402609a2cc6b4a32f9a1e41624a710
firegate666 <firegate666>
2006-09-06 08:20:59 +0000

    fix update

--- install/mysql/sql025.sql
+++ install/mysql/sql025.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `user` ADD `lastlogin` DATETIME NULL ,
 ADD `errorlogins` BIGINT NOT NULL DEFAULT '0',
-ADD `hash` VARCHAR( 100 ) NOT NULL ;
+ADD `hash` VARCHAR( 100 ) NOT NULL DEFAULT '';
 
 INSERT INTO dbversion(sql_id, sql_subid) VALUES (25, 0);
