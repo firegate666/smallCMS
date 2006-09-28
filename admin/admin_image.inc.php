6bb9e1c55b061c729d1ca18de8197216ada7212c
firegate666 <firegate666>
2006-09-28 07:11:19 +0000

    add upload-progressbar

--- admin/admin_image.inc.php
+++ admin/admin_image.inc.php
@@ -26,10 +26,18 @@ if (isset ($_REQUEST['img_upload']) && isset($HTTP_POST_FILES['filename'])) {
 }
 ?>
 <h3>Dateiverwaltung</h3>
-<a href="index.php?admin&image&img_upload">Upload</a> /
+<div id="myform"><a href="index.php?admin&image&img_upload">Upload</a> /
 <a href="index.php?admin&image&img_show">Anzeigen</a>
   <? if(isset($img_upload)) { ?>
-	<form action="index.php" enctype="multipart/form-data" method="post">
+	<form action="index.php" enctype="multipart/form-data" method="post" name="fupload">
+		<script type="text/javascript">
+			function upload() {
+				document.getElementById('msg').innerHTML = 'uploading';
+				document.getElementById('bar').style.display = 'block';
+				document.getElementById('myform').style.display = 'none';
+				document.forms['fupload'].submit();
+			}
+		</script>
 		<input type="hidden" name="img_upload"/>
 		<input type="hidden" name="image"/>
 		<input type="hidden" name="admin"/>
@@ -39,10 +47,11 @@ if (isset ($_REQUEST['img_upload']) && isset($HTTP_POST_FILES['filename'])) {
 			</tr>
 			<tr><td>Dateiname</td><td><input type="file" name="filename"/></td></tr>
 			<tr><td>Bildname </td><td><input type="text" name="img_name"/></td></tr>
-			<tr><td><td><input type="submit" value="Upload"/></td></tr>
+			<tr><td><td><input type="button" value="Upload" onclick="upload()"/></td></tr>
        </table>
-	</form>
-	<?=$msg?>
+	</form></div>
+	<div id="bar" style="display:none"><img src="img/progressbar.gif" alt="uploadbar"/></div>
+	<div id="msg"><?=$msg?></div>
   <? } ?>
   <? if(isset($img_show)) {
   		$image = new Image();
