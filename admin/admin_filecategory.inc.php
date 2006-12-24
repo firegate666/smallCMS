8425dc6f7e6d0724378239fc1c8d008d9fc408f5
root <root>
2006-12-24 01:09:22 +0000

    fix

--- admin/admin_filecategory.inc.php
+++ admin/admin_filecategory.inc.php
@@ -17,7 +17,7 @@ if (isset($_REQUEST['store'])) {
 
 <h3>Filecatregory Configuration</h3>
 	<? if(!isset($_REQUEST['id'])) { ?>
-		<a href="index.php?admin&type=filecategory&id=">Neue Filecategory anlegen</a>
+		<a href="index.php?admin&filecategory&id=">Neue Filecategory anlegen</a>
 		<table class="adminlist" width="100%">
 			<tr>
 				<th width="40%">Name</th>
@@ -32,7 +32,7 @@ if (isset($_REQUEST['store'])) {
 					<td width="40%"><?=$item['name']?></td>
 					<td width="40%"><?=$item['parent']?></td>
 					<td width="20%">
-						<a href='?admin&type=filecategory&id=<?=$item['id']?>'>
+						<a href='?admin&filecategory&id=<?=$item['id']?>'>
 							<img src='img/edit.gif' border='0' alt='Edit'/>
 						</a>
 						<img src='img/delete.gif' border='0' alt='Delete'/>
@@ -48,7 +48,7 @@ if (isset($_REQUEST['store'])) {
 				<?$obj = new Filecategory($_REQUEST['id']);?>
 				<input type="hidden" name="admin"/>
 				<input type="hidden" name="id" value="<?=$obj->get('id')?>"/>
-				<input type="hidden" name="type" value="filecategory"/>
+				<input type="hidden" name="filecategory"/>
 				<table class="adminedit" width="100%">
 					<tr>
 						<th colspan="2"><h3>Bearbeiten/Anlegen</h3></th>
