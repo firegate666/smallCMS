a2a4caed406172cfba1244cd2bfc93b5c04871e7
root <root>
2007-02-12 12:22:10 +0000

    add filecache handling

--- include/classes/lib/All.inc.php
+++ include/classes/lib/All.inc.php
@@ -1,6 +1,7 @@
 <?php
 	$d = dirname(__FILE__).'/';
 
+	require_once $d.'Cache.inc.php';
 	require_once $d.'TSP.inc.php';
 	require_once $d.'XML.inc.php';
 
---
+++ include/classes/lib/Cache.inc.php
@@ -0,0 +1,16 @@
+<?
+class Cache {
+	public static function read($key) {
+		if (file_exists('./cache/files/'.$key))
+			return file_get_contents('./cache/files/'.$key);
+		else
+			return false;
+	}
+	
+	public static function write($key, $value) {
+		if (!is_dir('./cache/files/'))
+			mkdir('./cache/files/', 0777);
+		file_put_contents('./cache/files/'.$key, $value);
+	}
+}
+?>
