556379148d4934dff54b74d00b589b60d901447b
root <root>
2007-04-17 07:11:28 +0000

    *** empty log message ***

--- include/classes/w40k/BattleType.inc.php
+++ include/classes/w40k/BattleType.inc.php
@@ -67,7 +67,6 @@ class BattleType extends W40K {
 				return array("Only one step deep in parents is allowed. Chosen parent is child too.");
 		} else
 			$vars['parent'] = null;
-		print_a($vars);
 		return parent::parsefields($vars);
 	}
 	
