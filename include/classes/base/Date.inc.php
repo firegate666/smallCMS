669b0b4e8ebe0fe46c421e430e671731b64dc6f6
root <root>
2007-08-19 16:35:35 +0000

    set static

--- include/classes/base/Date.inc.php
+++ include/classes/base/Date.inc.php
@@ -4,19 +4,19 @@
 
 
 class Date {
-	
+
 	/**
 	* get actual date formatted
 	*
 	* @param	String	$formatstring	format date, see php doc
 	* @return	String	formatted date
 	*/
-	public function now($formatstring = '') {
+	public static function now($formatstring = '') {
 		if(empty($formatstring)) {
 			$formatstring = Setting::read("timestampformat");
 		}
 		return strftime("$formatstring", time());
 	}
-	
+
 }
 ?>
