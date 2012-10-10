dcefe2a65e807076be681493e7b9d65d81efb47c
Marco Behnke <marco@behnke.biz>
2012-10-10 13:18:51 +0200

    add meaningful default

--- include/classes/base/Date.inc.php
+++ include/classes/base/Date.inc.php
@@ -13,7 +13,7 @@ class Date {
 	 * @param	String	$formatstring	format date, see php doc
 	 * @return	String	formatted date
 	 */
-	public static function now($formatstring = '') {
+	public static function now($formatstring = '%Y-%m-%d %H:%M:%S') {
 		if (empty($formatstring)) {
 			$formatstring = Setting::read("timestampformat");
 		}
