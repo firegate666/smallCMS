02c7f249ad37033c19dcecbafcef1c5962d26f95
Marco Behnke <marco@behnke.biz>
2012-10-11 15:19:17 +0200

    fix dependency calculation

--- include/classes/game/Techtree.inc.php
+++ include/classes/game/Techtree.inc.php
@@ -313,18 +313,38 @@ class TTExplored extends AbstractClass {
 
 		// ignore known techs
 		if (!empty($techids))
-			$techids = 'AND entry_id NOT IN (' . implode(',', $techids) . ')';
+			$techids = 'AND d.entry_id NOT IN (' . implode(',', $techids) . ')';
 		// ignore dependencies of running techs
 		if (!empty($runningtechs))
 			$techids .= ' AND dependson_id NOT IN (' . implode(',', $runningtechs) . ')';
 		if (empty($spieler_id))
 			$spieler_id = SeaWars::player();
-		$spieler_id = $mysql->escape($spieler_id);
-		$query = "SELECT *, COUNT(`techtree_entry_id`) AS erfuellt, COUNT(*) AS dependencies
+
+		$spieler_id = intval($spieler_id);
+		/*$query = "SELECT *, COUNT(`techtree_entry_id`) AS erfuellt, COUNT(*) AS dependencies
 					FROM `ttentrydependson`
 					LEFT JOIN `ttexplored` ON `dependson_id`=`techtree_entry_id`
     				WHERE spieler_id=$spieler_id GROUP BY `ttentrydependson`.`entry_id`
-	  				HAVING dependencies=erfuellt $techids;";
+	  				HAVING dependencies=erfuellt $techids;";*/
+
+		$query = "SELECT
+			e.id,
+			count(d.id) as dependencies,
+			count(ex.id) as dependencies_met
+		FROM
+			ttentry e
+			LEFT OUTER JOIN ttentrydependson d ON (d.entry_id = e.id)
+			LEFT OUTER JOIN ttexplored ex ON (ex.techtree_entry_id = d.dependson_id AND ex.finished = 1)
+		WHERE
+			(spieler_id = {$spieler_id} OR spieler_id IS NULL)
+			{$techids}
+		GROUP BY
+			e.id
+		HAVING
+			dependencies = dependencies_met
+		;";
+
+
 		$temp = $mysql->select($query, true);
 		$result = array();
 		if (is_array($runningtechs))
